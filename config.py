57aba730a27f4e3ebbc70136b1d8bd4e34f4857e
WIMED PROGRAMMING <alightmotionzann@gmail.com>
2026-08-14 02:42:44 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
+import os
 
+BOT_TOKEN = os.getenv("8225223905:AAEc_OzKG2ecjHjnAXgVJZSOme5ss3hbZLM", "")
+OWNER_ID = int(os.getenv("6095762919", "0"))
+
+ADMIN_LEVELS = {
+    "moderator": 5,
+    "admin": 10,
+    "superadmin": 50,
+    "owner": 100
+}
+
+B = "----------------------------------------"
